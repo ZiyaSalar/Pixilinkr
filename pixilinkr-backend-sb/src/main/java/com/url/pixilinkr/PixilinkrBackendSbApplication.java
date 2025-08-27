00b44e6579935247d99782557784ace740a7c959
Sayyad Ziya <sayyadziyu07@gmail.com>
2025-08-27 19:03:52 +0530

    backend-setup

---
+++ pixilinkr-backend-sb/src/main/java/com/url/pixilinkr/PixilinkrBackendSbApplication.java
@@ -0,0 +1,14 @@
+package com.url.pixilinkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixilinkrBackendSbApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello from ziya");
+        SpringApplication.run(PixilinkrBackendSbApplication.class, args);
+	}
+
+}
---
+++ pixilinkr-backend-sb/src/test/java/com/url/pixilinkr/PixilinkrBackendSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.pixilinkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixilinkrBackendSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
